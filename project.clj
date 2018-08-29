d52e352fe1338e0a6ea7411d0cdd8ff71400115e
Boaz Rosenan <brosenan@gmail.com>
2018-08-29 08:28:23 +0300

    Version 0.4.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject brosenan/lambdakube "0.4.1-SNAPSHOT"
+(defproject brosenan/lambdakube "0.4.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
