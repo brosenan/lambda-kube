12a1ac0c738f3ccfec0da323bdf645394ee415a5
Boaz Rosenan <brosenan@gmail.com>
2018-10-08 20:31:05 +0300

    Version 0.9.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject brosenan/lambdakube "0.9.3-SNAPSHOT"
+(defproject brosenan/lambdakube "0.9.3"
   :description "A Clojure library for generating Kubernetes API objects "
   :url "https://github.com/brosenan/lambda-kube"
   :license {:name "Eclipse Public License"
