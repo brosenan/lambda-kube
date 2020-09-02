2c036fe755f4ac7f665a9f395457afc9deb22c04
Nate Jones <nate@endot.org>
2020-09-02 14:31:01 -0700

    Update io.forward/yaml dependency.
    With 1.0.9, I was getting the errors from https://github.com/owainlewis/yaml/issues/28.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [io.forward/yaml "1.0.9"]
+                 [io.forward/yaml "1.0.10"]
                  [aysylu/loom "1.0.1"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/data.xml "0.0.8"]
