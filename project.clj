93e2556237a1f4e6f9986f7b7436b12fb982fb3d
Nate Jones <nate@endot.org>
2020-09-03 08:44:52 -0700

    Update remaining dependencies to the latest

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "https://github.com/brosenan/lambda-kube"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [io.forward/yaml "1.0.10"]
-                 [aysylu/loom "1.0.1"]
-                 [org.clojure/data.json "0.2.6"]
+                 [aysylu/loom "1.0.2"]
+                 [org.clojure/data.json "1.0.0"]
                  [org.clojure/data.xml "0.0.8"]
-                 [digest "1.4.8"]]
+                 [digest "1.4.9"]]
   :profiles {:dev {:dependencies [[midje "1.9.9"]]
                    :plugins [[lein-midje "3.2.1"]
                              [lein-auto "0.1.3"]]}}
