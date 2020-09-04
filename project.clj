89cea5246f5ab95fd827582d7a1800e4eb65cdb6
Nate Jones <nate@endot.org>
2020-09-04 12:47:50 -0700

    Update io.forward/yaml dependency. (#3)
    * Update io.forward/yaml dependency.
    
    With 1.0.9, I was getting the errors from https://github.com/owainlewis/yaml/issues/28.
    
    * Bump travis to JDK 11, transitive dependency snakeyaml needs it
    
    * Update midje dependency
    
    * Update remaining dependencies to the latest

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "https://github.com/brosenan/lambda-kube"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [io.forward/yaml "1.0.9"]
-                 [aysylu/loom "1.0.1"]
-                 [org.clojure/data.json "0.2.6"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [io.forward/yaml "1.0.10"]
+                 [aysylu/loom "1.0.2"]
+                 [org.clojure/data.json "1.0.0"]
                  [org.clojure/data.xml "0.0.8"]
-                 [digest "1.4.8"]]
-  :profiles {:dev {:dependencies [[midje "1.9.2"]]
+                 [digest "1.4.9"]]
+  :profiles {:dev {:dependencies [[midje "1.9.9"]]
                    :plugins [[lein-midje "3.2.1"]
                              [lein-auto "0.1.3"]]}}
   :deploy-repositories [["releases" :clojars]])
